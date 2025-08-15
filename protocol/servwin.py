9099ac56d2be26af1dea72e0b5514ac96e00792b
fishBone000 <FishBoneEK@outlook.com>
2025-08-15 16:45:43 +0000

    Renew servwin

--- protocol/servwin.py
+++ protocol/servwin.py
@@ -1,19 +1,46 @@
-import struct
-from dataclasses import dataclass
+import socket
+from Crypto.PublicKey.ECC import EccKey
+from Crypto.Signature import eddsa
+from Crypto.Hash import SHA512
+import kex
+from protocol.transport import NonceManager
+from transport import TCPTransport
+from construct import Struct, Int32ub, Array, Int16ub
 
-@dataclass
-class ServiceWindow:
-    ports: tuple[int]
-    sec_til_expire: int
-    sec_til_next_win: int
 
-    def pack(self):
-        return struct.pack(f">H{len(self.ports)}HII", len(self.ports), *self.ports, self.sec_til_expire,
-                           self.sec_til_next_win)
+def get_struct(num_ports):
+    return Struct(
+        "sec_til_expire" / Int32ub,
+        "ports" / Int16ub[num_ports]
+    )
 
-    @classmethod
-    def unpack(cls, data):
-        (num_ports,) = struct.unpack(">H", data[:2])
-        ports = struct.unpack(f">{num_ports}H", data[2:2 + 2 * num_ports])
-        (sec_til_expire, sec_til_next_win) = struct.unpack(">II", data[2 + 2 * num_ports:])
-        return cls(ports, sec_til_expire, sec_til_next_win)
+
+def build(sec_til_expire: int, ports: list[int]) -> bytes:
+    s = get_struct(len(ports))
+    return s.build({"sec_til_expire": sec_til_expire, "ports": ports})
+
+
+def parse(data: bytes) -> (int, list[int]):
+    sz = len(data)
+    num_ports = int((sz - 4) / 2)
+    if (sz - 4) % 2:
+        raise ValueError("bad format")
+    r = get_struct(num_ports).parse(data)
+    return r.sec_til_expire, r.ports
+
+
+def query(host: str, port: int, priv_key: EccKey, host_pub_key: EccKey | None, nonce_mng: NonceManager) -> (int,
+                                                                                                            list[int]):
+    s = socket.create_connection((host, port), timeout=5)
+    (k1, k2, host_pub_key) = kex.client_to_server(s, host_pub_key)
+    tp = TCPTransport(s, k1, k2, nonce_mng)
+
+    # Do authentication
+    pub_key = priv_key.public_key()
+    signer = eddsa.new(priv_key, mode="rfc8032")
+    h = SHA512.new(k1 + k2 + pub_key.export_key(format="raw")).digest()
+    sign = signer.sign(h)
+    tp.sendall(pub_key.export_key(format="raw") + sign)
+
+    data = tp.recv()
+    return parse(data)
