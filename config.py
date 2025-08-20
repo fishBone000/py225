cf0abbe249c2f2bd3bb5c88f3e2f4d5978b62b22
fishBone000 <FishBoneEK@outlook.com>
2025-08-20 15:41:25 +0000

    Add config populate automation and unit test

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
+from __future__ import annotations
+
 import os
 import sys
+import typing
 from dataclasses import dataclass, field
+from types import GenericAlias
 from typing import Literal
 
 import yaml
@@ -8,64 +12,167 @@ from Crypto.PublicKey import ECC
 from Crypto.PublicKey.ECC import EccKey
 
 
+class BaseConfig(yaml.YAMLObject):
+    yaml_loader = yaml.SafeLoader
+    yaml_dumper = yaml.SafeDumper
+    inherited_fields = set()
+
+    def inherit(self, parent: BaseConfig):
+        """
+        Inherit fields from parent config if the field is not set in this object and is set in parent config
+        :param parent: Config that is 1 level up.
+        """
+        for ihf in self.inherited_fields:
+            if hasattr(parent, ihf) and (not hasattr(self, ihf) or getattr(self, ihf) is None):
+                setattr(self, ihf, getattr(parent, ihf))
+
+    def post_load(self):
+        """
+        Iterate over all fields that are instance of ``AbstractConfig``,
+        then calls their ``inherit`` and  ``post_load`` methods.
+
+        Override this method to implement post loading logic.
+        """
+        for k in self.__dict__:
+            v = self.__dict__[k]
+            if isinstance(v, BaseConfig):
+                try:
+                    v.inherit(self)
+                    v.post_load()
+                except Exception as e:
+                    e.add_note(f"In field {k}")
+                    raise
+            elif isinstance(v, list):
+                for i in range(len(v)):
+                    inst = v[i]
+                    if isinstance(inst, BaseConfig):
+                        try:
+                            inst.inherit(self)
+                            inst.post_load()
+                        except Exception as e:
+                            e.add_note(f"In #{i} item of field {k}")
+                            raise
+
+    def validate(self):
+        hints = typing.get_type_hints(type(self))
+        for k in hints:
+            hint = hints[k]
+
+            # If attribute not present
+            if not hasattr(self, k):
+                if isinstance(None, hint):  # If field is optional
+                    setattr(self, k, None)
+                else:
+                    raise TypeError(f"Field {k} is mandatory")
+
+            v = getattr(self, k)
+            err = TypeError(f"Field {k}: expected type {hint}, got {type(v)}")
+
+            # If is generic type
+            if isinstance(hint, GenericAlias):
+                o = typing.get_origin(hint)
+                if not isinstance(v, o):
+                    raise err
+                match o:
+                    case _ if o is list:
+                        arg = typing.get_args(hint)[0]
+                        if not all((isinstance(i, arg) for i in v)):
+                            raise ValueError(f"Field {k} expects a list of {arg}")
+
+                        if issubclass(arg, BaseConfig):
+                            for i in range(len(v)):
+                                inst = v[i]
+                                try:
+                                    inst.validate()
+                                except Exception as e:
+                                    e.add_note(f"In #{i} of field {k}")
+                                    raise
+                    case _:
+                        raise err
+                continue
+
+            # If is primitive types or classes
+            if not isinstance(v, hint):
+                raise err
+            if isinstance(v, BaseConfig):
+                try:
+                    v.validate()
+                except Exception as e:
+                    e.add_note(f"In field {k}")
+                    raise
+
+
 @dataclass
-class ServerRecord(yaml.YAMLObject):
+class ServerRecord(BaseConfig):
     host: str
     port: int
-    private_key: EccKey = field(repr=False)
-    host_public_key: EccKey | None = None
-    yaml_loader = yaml.SafeLoader
+    private_key: EccKey | str | None = field(repr=False)
+    host_public_key: EccKey | str | None = None
     yaml_tag = "!ServerRecord"
+    inherited_fields = {"private_key"}
 
-    def __setstate__(self, state):
-        self.__dict__.update(state)
+    def validate(self):
+        super().validate()
+        if not 0 < self.port < 65536:
+            raise ValueError("invalid port", self.port)
 
-        if not hasattr(self, "private_key"):
-            self.private_key = None
-        if not hasattr(self, "host_public_key"):
-            self.host_public_key = None
+    def post_load(self):
+        if self.private_key is None:
+            raise ValueError(f"Field private_key is mandatory")
 
-        if type(self.private_key) is str:
-            self.private_key = ECC.import_key(self.private_key)
-            if self.private_key.curve != "Ed25519":
-                raise ValueError("Only Ed25519 keys are supported")
-        if type(self.host_public_key) is str:
-            self.host_public_key = ECC.import_key(self.host_public_key)
-            if self.host_public_key.curve != "Ed25519":
-                raise ValueError("Only Ed25519 keys are supported")
+        if not isinstance(self.private_key, EccKey):
+            try:
+                self.private_key = ECC.import_key(self.private_key)
+                if self.private_key.curve != "Ed25519":
+                    raise ValueError("Key must be Ed25519 key")
+            except Exception as e:
+                e.add_note("field private_key must be importable Ed25519 private key in ASCII")
+                raise
 
-        if not 0 < self.port < 65535:
-            raise ValueError(f"invalid port {self.port}")
+        try:
+            if self.host_public_key is not None:
+                self.host_public_key = ECC.import_key(self.host_public_key)
+                if self.host_public_key.curve != "Ed25519":
+                    raise ValueError("Key must be Ed25519 key")
+        except Exception as e:
+            e.add_note("field host_public_key must be importable Ed25519 public key in ASCII")
+            raise
+
+    def __getstate__(self):
+        d = self.__dict__.copy()
+        d["private_key"] = self.private_key.export_key(format="PEM")
+        if self.host_public_key is not None:
+            d["host_public_key"] = self.host_public_key.export_key(format="PEM")
+        return d
 
 
 @dataclass
-class Client(yaml.YAMLObject):
+class Client(BaseConfig):
     servers: list[ServerRecord]
     log: str
     verbosity: str
     listen_ip: str
     listen_port: int
-    private_key: EccKey | None = field(repr=False)
-    yaml_loader = yaml.SafeLoader
+    private_key: EccKey | str | None = field(repr=False)
     yaml_tag = "!Client"
 
-    def __setstate__(self, state):
-        self.__dict__.update(state)
+    def post_load(self):
+        if self.private_key is not None:
+            msg = "optional field private_key must be importable Ed25519 private key in ASCII or unset"
+            try:
+                self.private_key = ECC.import_key(self.private_key)
+            except Exception as e:
+                e.add_note(msg)
+                raise
 
-        if not hasattr(self, "private_key"):
-            self.private_key = None
+        super().post_load()
 
-        if type(self.private_key) is str:
-            self.private_key = ECC.import_key(self.private_key)
-            if self.private_key.curve != "Ed25519":
-                raise ValueError("Only Ed25519 keys are supported")
+    def __getstate__(self):
+        d = self.__dict__.copy()
+        if self.private_key is not None:
+            d["private_key"] = self.private_key.export_key(format="PEM")
+        return d
 
-        for rec in self.servers:
-            if rec.private_key is None:
-                if self.private_key is None:
-                    raise ValueError(f"no private key specified for {rec.host} port {rec.port}")
-                if self.private_key is not None:
-                    rec.private_key = self.private_key
 
 
 def get_default_cfg_paths(name: Literal["py225", "py225d", "py225-gui"]) -> list[str]:
--- test/test_config.py
+++ test/test_config.py
@@ -1,58 +1,70 @@
 import unittest
 
 import yaml
-from Crypto.PublicKey import ECC
+from Crypto.PublicKey.ECC import generate, EccKey
 
 import config
 
 
 class TestLoadingYamlConfig(unittest.TestCase):
-    def test_client_simple(self):
+    def test_client_dump(self):
         """
-        Tests if ``_load_yaml`` loads a simple YAML client config
+        Tests config.Client dumps correctly
         """
-        cfg = """
-        !Client
-        servers: 
-          - !ServerRecord
-            host: example.com
-            port: 9000
-          - !ServerRecord
-            host: s1.example.com
-            port: 9005
-            private_key: |
-              -----BEGIN PRIVATE KEY-----
-              MC4CAQAwBQYDK2VwBCIEING/rSmw5/fC905ZLzmdT+3L2L4RgFeORXSN7iMdxyXU
-              -----END PRIVATE KEY-----
-            host_public_key: |
-              -----BEGIN PUBLIC KEY-----
-              MCowBQYDK2VwAyEAmbPDBnrb750SqEzzNNMSSNdPxRjg+PrC4jJ3hocADoo=
-              -----END PUBLIC KEY-----
-        log: 
-          - syslog
-        private_key: |
-          -----BEGIN PRIVATE KEY-----
-          MC4CAQAwBQYDK2VwBCIEIKipm4URANeuFgVgeIqzIMIZGdyw2UEMnqCbupcGep73
-          -----END PRIVATE KEY-----
-        """
+        def gen():
+            return generate(curve="Ed25519")
 
-        expected_servers = [
-            config.ServerRecord("example.com", 9000, ECC.import_key("""
-        -----BEGIN PRIVATE KEY-----
-        MC4CAQAwBQYDK2VwBCIEIKipm4URANeuFgVgeIqzIMIZGdyw2UEMnqCbupcGep73
-        -----END PRIVATE KEY-----""".strip()), None),
-            config.ServerRecord("s1.example.com", 9005, ECC.import_key("""
-                    -----BEGIN PRIVATE KEY-----
-                   MC4CAQAwBQYDK2VwBCIEING/rSmw5/fC905ZLzmdT+3L2L4RgFeORXSN7iMdxyXU
-                   -----END PRIVATE KEY-----""".strip()), ECC.import_key("""
-                   -----BEGIN PUBLIC KEY-----
-                   MCowBQYDK2VwAyEAmbPDBnrb750SqEzzNNMSSNdPxRjg+PrC4jJ3hocADoo=
-                   -----END PUBLIC KEY-----""".strip()))
-        ]
-        expected = config.Client(expected_servers, ["syslog"], ECC.import_key("""
-        -----BEGIN PRIVATE KEY-----
-        MC4CAQAwBQYDK2VwBCIEIKipm4URANeuFgVgeIqzIMIZGdyw2UEMnqCbupcGep73
-        -----END PRIVATE KEY-----""".strip()))
+        cfg = config.Client(
+            [config.ServerRecord("google.com", 443, gen(), gen().public_key()),
+             config.ServerRecord("cloudflare.com", 888, gen(), None)
+             ],
+            "stderr", "warning", "127.0.0.1", 1080, gen()
+        )
+        yml = yaml.safe_dump(cfg)
+        print(yml)
+        cfg2 = yaml.safe_load(yml)
+        cfg2.validate()
+        cfg2.post_load()
+        self.assertEqual(cfg, cfg2)
 
-        yml = yaml.safe_load(cfg)
-        self.assertEqual(yml, expected, "unexpected parse result")
+    def test_client_load(self):
+        def gen():
+            return generate(curve="Ed25519")
+        default_key = gen()
+        priv_keys = [default_key, gen()]
+        pub_keys = [gen(), None]
+        expected = config.Client(
+            [config.ServerRecord("google.com", 443, priv_keys[0], pub_keys[0]),
+             config.ServerRecord("cloudflare.com", 888, priv_keys[1], pub_keys[1])
+             ],
+            "stderr","warning", "127.0.0.1", 1080, default_key
+        )
+
+        def export(k: EccKey, indent: int = 1):
+            s = k.export_key(format="PEM")
+            return ("\n\n" + "  "*indent).join(s.split("\n"))
+
+        yml = f"""
+!Client
+listen_ip: 127.0.0.1
+listen_port: 1080
+log: stderr
+private_key: '{export(default_key)}'
+servers:
+- !ServerRecord
+  host: google.com
+  host_public_key: '{export(pub_keys[0], 2)}'
+  port: 443
+- !ServerRecord
+  host: cloudflare.com
+  host_public_key: null
+  port: 888
+  private_key: '{export(priv_keys[1], 2)}'
+verbosity: warning
+"""
+        print(yml)
+        cfg = yaml.safe_load(yml)
+        cfg.validate()
+        cfg.post_load()
+
+        self.assertEqual(expected, cfg)
